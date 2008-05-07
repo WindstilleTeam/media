0f2477fbef9006950b80b729795969ff98af13f4
Ingo Ruhnke <grumbel@gmail.com>
2008-05-07 20:45:27 +0000

    Added build script

--- wiki/wiki2html.py
+++ wiki/wiki2html.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
 
+from genshi.template import MarkupTemplate
 import creoleparser
 import sys
 
+def file2string(filename):
+    f = open(filename)
+    s = f.read()
+    f.close()   
+    return s
+
 def main(args):
     if args == []:
         print "Usage: wiki2html.py FILENAME..."
     else:
+        creole_parser = creoleparser.Parser(dialect=creoleparser.Creole10(
+                wiki_links_base_url='',
+                wiki_links_path_func=lambda pagename: pagename + ".html",
+                wiki_links_space_char=' ',
+                # no_wiki_monospace=True,
+                use_additions=True
+                ))
+	
+
         for filename in args:
-            f = open(filename)
-            print creoleparser.text2html(f.read())
-            f.close()
+            tmpl = MarkupTemplate(file2string("template.xml"))
+            print tmpl.generate(body = creole_parser.generate(file2string(filename)))
+
             
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
