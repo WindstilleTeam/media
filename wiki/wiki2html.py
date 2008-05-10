817f9b1e2773f36439aca21b8ced373e99cfb705
Ingo Ruhnke <grumbel@gmail.com>
2008-05-10 16:11:03 +0000

    Some more cleanup

--- wiki/wiki2html.py
+++ wiki/wiki2html.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from creoleparser import text2html
 from genshi.template import MarkupTemplate
 import genshi.builder as bldr
 import creoleparser
@@ -15,41 +14,48 @@ def file2string(filename):
     f.close()   
     return s
 
-def macro_func(name,arg_string,body):
-    if name == "comment":
-        return bldr.tag.div(bldr.tag(bldr.tag.div(arg_string[1:], class_='user'),
-                                     bldr.tag.div(text2html.generate(body), class_= "body")), # FIXME: Recursive comments don't really work 
-                            class_='comment')
-    elif name == "ind":
-        return bldr.tag.div(text2html.generate(body), class_='indent' + arg_string[1:])
-    else:
-        raise Exception("Invalid Macro name: %s" % name)
+class Wiki2HTML:
+    def macro_func(self, name, arg_string, body):
+        if body == None:
+            return ""
 
-def talk_links_funcs(str):
-    return "Talk%3a" + str + ".html"
+        if name == "comment":
+            body = self.creole_parser.generate(body)
+            return bldr.tag.div(bldr.tag(bldr.tag.div(arg_string[1:], class_='user'),
+                                         bldr.tag.div(body, class_= "body")), # FIXME: Recursive comments don't really work 
+                                class_='comment')
+        elif name == "ind":
+            body = self.creole_parser.generate(body)
+            return bldr.tag.div(body, class_='indent' + arg_string[1:])
+        else:
+            raise Exception("Invalid Macro name: %s" % name)
 
-def main(args):
-    if args == []:
-        print "Usage: wiki2html.py FILENAME..."
-    else:
-        creole_parser = creoleparser.Parser(dialect=creoleparser.Creole10(
-                wiki_links_base_url='',
-                wiki_links_path_func=lambda pagename: pagename + ".html",
-                wiki_links_space_char=' ',
-                # no_wiki_monospace=True,
-                use_additions=True,
-                macro_func = macro_func,
-                interwiki_links_funcs={'Talk' : talk_links_funcs }
-                ))
-	
+    def talk_links_funcs(self, str):
+        return "Talk%3a" + str + ".html"
 
-        for filename in args:
-            tmpl = MarkupTemplate(file2string("template.xml"))
-            print tmpl.generate(body = creole_parser.generate(file2string(filename)),
-                                title = filename.replace(".wiki", ""))
+    def main(self, args):
+        if args == []:
+            print "Usage: wiki2html.py FILENAME..."
+        else:
+            self.creole_parser = creoleparser.Parser(dialect=creoleparser.Creole10(
+                    wiki_links_base_url   = '',
+                    wiki_links_path_func  = lambda pagename: pagename + ".html",
+                    wiki_links_space_char = ' ',
+                    # no_wiki_monospace   = True,
+                    use_additions         = True,
+                    macro_func            = self.macro_func,
+                    interwiki_links_funcs={'Talk' : self.talk_links_funcs }
+                    ))
+
+
+            for filename in args:
+                tmpl = MarkupTemplate(file2string("template.xml"))
+                print tmpl.generate(body = self.creole_parser.generate(file2string(filename)),
+                                    title = filename.replace(".wiki", ""))
 
             
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    wiki2html = Wiki2HTML()
+    sys.exit(wiki2html.main(sys.argv[1:]))
 
 # EOF #
