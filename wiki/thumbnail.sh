3bec9e56dda49d42d17d2101d2d6e2e11651d51a
Ingo Ruhnke <grumbel@gmail.com>
2015-03-22 17:36:57 +0100

    Migrated wiki to git@github.com:WindstilleTeam/windstille.wiki.git

--- wiki/thumbnail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in "$@"; do
-    echo "Thumbnailing $i"
-    convert -scale 200x200 -quality 85% "$i" "thumbnails/$(basename "$i")"
-done
-
-# EOF #
--- wiki/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rsync -vrPcLC  build/ grumbel@shell.berlios.de:/home/groups/windstille/htdocs/wiki
-
-# EOF #
